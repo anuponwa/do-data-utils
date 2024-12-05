364eba214c2f434ef8019c06708ab52eafe86904
Anupong Wannakrairot <anuponwa@scg.com>
2024-12-05 16:04:47 +0700

    Change import behavior

--- datautils/__init__.py
+++ datautils/__init__.py
@@ -9,17 +9,9 @@
 import warnings
 
 
-from .google import (
-    get_secret,
-    set_gcs_client,
-    gcs_exists,
-    gcs_listdirs,
-    gcs_listfiles,
-    gcs_to_dict,
-    gcs_to_file
-)
+import google
 
 try:
-    from .azure import authen_databrick_sql, databricks_to_df
+    import azure
 except:
     warnings.warn('Azure utilities not imported. If you want to use them, please install them via: pip install "git+https://github.com/anuponwa/datautils.git@<version>#egg=datautils[azure]"')
