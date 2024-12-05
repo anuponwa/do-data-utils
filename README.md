d34630e4d463aba2f2f6b636f0b6ebaf46ddc4d0
Anupong Wannakrairot <anuponwa@scg.com>
2024-12-05 15:54:06 +0700

    Import error handling in __init__.py and update README

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ This package provides you the functionalities to connect to different cloud sour
 ## Installation
 
 ```bash
-pip install git+https://github.com/anuponwa/datautils.git@1.0.0
+pip install "git+https://github.com/anuponwa/datautils.git@<version>"
+```
+For example,
+```bash
+pip install "git+https://github.com/anuponwa/datautils.git@1.0.0"
 ```
 
 You can install extra dependencies by specifying the extra:
 ```bash
-pip install git+https://github.com/anuponwa/datautils.git@1.0.0[azure]
+pip install "git+https://github.com/anuponwa/datautils.git@1.0.0#egg=datautils[azure]"
 ```
+Extra options can be inspected in `setup.py` in the `extras_require` option.
--- datautils/__init__.py
+++ datautils/__init__.py
@@ -6,5 +6,18 @@
     datautils also comes with common data cleaning functions.
 """
 
-from .google import get_secret, set_gcs_client, gcs_exists, gcs_listdirs, gcs_listfiles, gcs_to_dict, gcs_to_file
-from .azure import authen_databrick_sql, databricks_to_df
+
+from .google import (
+    get_secret,
+    set_gcs_client,
+    gcs_exists,
+    gcs_listdirs,
+    gcs_listfiles,
+    gcs_to_dict,
+    gcs_to_file
+)
+
+try:
+    from .azure import authen_databrick_sql, databricks_to_df
+except:
+    raise ImportError('Azure utilities not imported. If you want to use them, please install them via: pip install "git+https://github.com/anuponwa/datautils.git@<version>#egg=datautils[azure]"')
