3c6631b8c29779c4943ff61f49cf6e26d45694e6
anuponwa <158556877+anuponwa@users.noreply.github.com>
2025-01-18 13:05:15 +0700

    Add functionalities to preprocessing, format and add tests (#46)

--- do_data_utils/preprocessing/email.py
+++ do_data_utils/preprocessing/email.py
@@ -3,22 +3,41 @@ from typing import Optional
 from .common import search_regexp
 
 
-def clean_email(email: str) -> Optional[str]:
-    """Cleans the e-mail
+def clean_email(email: str, delimiter: Optional[str] = None) -> Optional[str]:
+    """Cleans the e-mail.
 
     Parameters
     ----------
     email: str
-        E-mail string, e.g., somename@somedomain.com
+        E-mail string, e.g., somename@somedomain.com.
+
+    delimiter: str, default=None
+        A delimiter character to separate multiple emails.
+        If None, it expects that email is not delimited.
 
     Returns
     -------
     str
-        A valid e-mail string, else None
+        A valid e-mail string, else None.
     """
 
     if not email:
         return None
 
     email_pat = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}"
-    return search_regexp(pattern=email_pat, string=email)
+
+    if not delimiter:
+        return search_regexp(pattern=email_pat, string=email)
+
+    else:
+        ret = []
+        email_list = email.split(delimiter)
+        for e in email_list:
+            email_extracted = search_regexp(pattern=email_pat, string=e)
+            if email_extracted:
+                ret.append(email_extracted)
+
+        if ret:
+            return delimiter.join(ret)
+
+        return None
--- tests/preprocessing/test_email.py
+++ tests/preprocessing/test_email.py
@@ -11,3 +11,18 @@ def test_email(input, expected):
 
 def test_empty_email():
     assert clean_email("") is None
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ("anuponwa@scg.com|", "anuponwa@scg.com"),
+        (
+            "anuponwa@scg.com|xxx@xy|someone@domain.com",
+            "anuponwa@scg.com|someone@domain.com",
+        ),
+        ("anuponwa", None),
+    ],
+)
+def test_multiple_emails(input, expected):
+    assert clean_email(input, delimiter="|") == expected
